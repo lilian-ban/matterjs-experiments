ba1789bd796c500d2cce7972af97a9312255c0f5
Lilian Banchereau <contact@lilianbanchereau.fr>
2023-03-02 08:07:01 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+var engine = Engine.create()
+
+var render = Render.create({
+    element: document.body,
+    engine: engine
+})
+
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
